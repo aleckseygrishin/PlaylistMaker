e7bd6693f515b4a2684eba688616c7d038ab56f7
aleckseygrishin <combine696@yandex.ru>
2025-05-13 20:49:26 +0300

    sprint 9. add logic SearchActivity + layout search

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,13 +1,72 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
+
+    private var enterText: String = ENEMY_TEXT
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_search)
+
+        val arrowBack = findViewById<MaterialToolbar>(R.id.arrow_back_search)
+        arrowBack.setNavigationOnClickListener {
+            finish()
+        }
+
+        val inputEditText = findViewById<EditText>(R.id.search_edit_text)
+        val clearButton = findViewById<ImageView>(R.id.clear_icon)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                enterText = s.toString()
+            }
+
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(ENTER_TEXT_KEY, enterText)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        enterText = savedInstanceState.getString(ENTER_TEXT_KEY, ENEMY_TEXT)
+    }
+
+
+    companion object {
+        const val ENTER_TEXT_KEY = "ENTER_TEXT_SEARCH"
+        const val ENEMY_TEXT = ""
     }
 }
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,17 +1,48 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.textview.MaterialTextView
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val arrowBack = findViewById<MaterialToolbar>(R.id.arrow_back)
+        val arrowBack = findViewById<MaterialToolbar>(R.id.arrow_back_settings)
         arrowBack.setNavigationOnClickListener {
             finish()
+        }
+
+        val shareApplicationView = findViewById<MaterialTextView>(R.id.share_application_activity_settings)
+        shareApplicationView.setOnClickListener {
+            val shareMessage = Intent(Intent.ACTION_SEND)
+            shareMessage.putExtra(Intent.EXTRA_TEXT, getString(R.string.android_developer_course_link))
+            shareMessage.setType("text/plain")
+            startActivity(shareMessage)
+        }
+
+        val writeHelpDesk = findViewById<MaterialTextView>(R.id.help_desk_activity_settings)
+        writeHelpDesk.setOnClickListener {
+            val sendMail = Intent(Intent.ACTION_SENDTO)
+            sendMail.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.theme_mail))
+            sendMail.putExtra(Intent.EXTRA_TEXT, getString(R.string.body_mail))
+            sendMail.setData(Uri.parse("mailto:${getString(R.string.email_student)}"))
+            startActivity(sendMail)
+        }
+
+        val userAgreement = findViewById<MaterialTextView>(R.id.user_agreement_activity_settings)
+        userAgreement.setOnClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(
+                        getString(R.string.user_agreement_link)
+                    ))
+            )
         }
     }
 }
